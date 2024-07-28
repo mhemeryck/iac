c64cb72066fb957b0973d26494d989f9a92a10dc
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-07-28 22:20:37 +0200

    more wekan

--- node/dns.tf
+++ node/dns.tf
@@ -1,11 +1,19 @@
 resource "hetznerdns_zone" "xyz" {
   name = "mhemeryck.xyz"
-  ttl  = 300
+  ttl  = 3600
 }
 
 resource "hetznerdns_record" "xyz" {
   zone_id = hetznerdns_zone.xyz.id
   name    = "@"
+  value   = hcloud_server.k3s.ipv4_address
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "hetznerdns_record" "wekan" {
+  zone_id = hetznerdns_zone.xyz.id
+  name    = "wekan"
   value   = hcloud_server.k3s.ipv4_address
   type    = "A"
   ttl     = 300
