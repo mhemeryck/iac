849bda8ff81b056b9dbdd5f017ed0ad0e047e69a
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2026-08-23 21:52:27 +0200

    Hand off Goalkeepr backup setup
    Keep application backup infrastructure with its owning repository.

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,6 @@
 
   env = {
     AWS_PROFILE                = "mhemeryck";
-    TF_VAR_billing_alert_email = config.secretspec.secrets.BILLING_ALERT_EMAIL;
     TF_VAR_do_token            = config.secretspec.secrets.DIGITALOCEAN_TOKEN;
     TF_VAR_hcloud_token        = config.secretspec.secrets.HCLOUD_TOKEN;
   };
