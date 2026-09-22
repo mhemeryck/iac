1ab57d79718901a947426af896e8311d6e027410
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2026-09-22 22:22:46 +0200

    Manage Wekan with Terraform (#31)
    * Add Wekan Terraform module
    
    Manage the Wekan Kubernetes resources from an isolated Terraform root.
    
    * Reference MongoDB service
    
    Keep Wekan connected when the MongoDB service name changes.
    
    * Configure Wekan kubeconfig
    
    Pass the SecretSpec kubeconfig path to the Wekan Terraform root.
    
    * Preserve Wekan runtime settings
    
    Avoid changing live pod defaults during Terraform adoption.
    
    * Remove Wekan manifest workflow
    
    Use Terraform as the sole source of truth for Wekan resources.

--- devenv.nix
+++ devenv.nix
@@ -11,6 +11,7 @@
     AWS_PROFILE = "mhemeryck";
     TF_VAR_do_token = config.secretspec.secrets.DIGITALOCEAN_TOKEN;
     TF_VAR_hcloud_token = config.secretspec.secrets.HCLOUD_TOKEN;
+    TF_VAR_kubeconfig_path = config.secretspec.secrets.KUBECONFIG;
     KUBECONFIG = config.secretspec.secrets.KUBECONFIG;
   };
 }
---
+++ wekan/main.tf
@@ -0,0 +1,209 @@
+resource "kubernetes_namespace_v1" "wekan" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_deployment_v1" "wekan" {
+  wait_for_rollout = false
+
+  metadata {
+    name      = "wekan"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "wekan"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "wekan"
+        }
+      }
+
+      spec {
+        automount_service_account_token = false
+        enable_service_links            = false
+
+        container {
+          name  = "wekan"
+          image = var.wekan_image
+
+          env {
+            name  = "MONGO_URL"
+            value = "mongodb://${kubernetes_service_v1.mongodb.metadata[0].name}/wekan"
+          }
+
+          env {
+            name  = "ROOT_URL"
+            value = "https://${var.host}"
+          }
+
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      spec[0].template[0].metadata[0].annotations["kubectl.kubernetes.io/restartedAt"],
+    ]
+  }
+}
+
+resource "kubernetes_service_v1" "wekan" {
+  wait_for_load_balancer = false
+
+  metadata {
+    name      = "wekan"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "wekan"
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "mongodb" {
+  metadata {
+    name      = "mongodb"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = var.mongodb_storage
+      }
+    }
+  }
+}
+
+resource "kubernetes_stateful_set_v1" "mongodb" {
+  wait_for_rollout = false
+
+  metadata {
+    name      = "mongodb"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+  }
+
+  spec {
+    replicas     = 1
+    service_name = kubernetes_service_v1.mongodb.metadata[0].name
+
+    selector {
+      match_labels = {
+        app = "mongodb"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "mongodb"
+        }
+      }
+
+      spec {
+        automount_service_account_token = false
+        enable_service_links            = false
+
+        container {
+          name  = "mongo"
+          image = var.mongodb_image
+
+          volume_mount {
+            name       = "mongodb"
+            mount_path = "/data/db"
+          }
+        }
+
+        volume {
+          name = "mongodb"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.mongodb.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "mongodb" {
+  wait_for_load_balancer = false
+
+  metadata {
+    name      = "mongodb"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "mongodb"
+    }
+
+    port {
+      port = 27017
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "wekan" {
+  metadata {
+    name      = "wekan"
+    namespace = kubernetes_namespace_v1.wekan.metadata[0].name
+
+    annotations = {
+      "cert-manager.io/cluster-issuer" = var.cluster_issuer
+    }
+  }
+
+  spec {
+    ingress_class_name = var.ingress_class_name
+
+    rule {
+      host = var.host
+
+      http {
+        path {
+          path_type = "ImplementationSpecific"
+
+          backend {
+            service {
+              name = kubernetes_service_v1.wekan.metadata[0].name
+
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+
+    tls {
+      hosts       = [var.host]
+      secret_name = var.tls_secret_name
+    }
+  }
+}
